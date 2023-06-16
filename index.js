f6243762722d33ded8fd17e9c0568c75d9ede7aa
Aman Malhotra <49841734+aman003malhotra@users.noreply.github.com>
2023-06-16 23:42:09 +0530

    live website V_1

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-var express = require('express'); 
+const { default: axios } = require('axios');
+var express = require('express');
+const session = require('express-session');
+const { Client, getTimeAgo, editorJsParse } = require('./config/client');
 require("dotenv").config();
+
+const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
+const cors = require('cors');
+
 var app = express();
 
+app.use(bodyparser.json({ limit: '50mb' }));
+app.use(bodyparser.urlencoded({ limit: '500mb', extended: true, parameterLimit: 5000000 }));
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cookieparser());
+
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+});
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+
 app.use(express.static('public'));
+const edjsParser = require("editorjs-parser");
 
 app.set('view engine', 'ejs');
 
@@ -29,7 +62,7 @@ const menu = [
     // },
     {
         name: 'About',
-        url: '/about'
+        url: '#about'
     },
     {
         name: 'Contact',
@@ -39,158 +72,195 @@ const menu = [
 
 const courses = [
     {
-        subject:"general_studies",
-        thumbnail:"course_1.png",
-        number_of_lessons:"120",
-        about_course:"Studying diverse art, modern &world history, post-Independence India, world & Indian geography, and Indian society's culture and structure.",
-        tutor:"Samriti talk",
-        prev_institude:"Drishti IAS",
-        student_enrolled:"160",
-        rating:5
+        subject: "General studies 1",
+        thumbnail: "course_1.png",
+        number_of_lessons: "120",
+        about_course: "Studying diverse art, modern &world history, post-Independence India, world & Indian geography, and Indian society's culture and structure.",
+        tutor: "Samriti talk",
+        prev_institude: "Drishti IAS",
+        student_enrolled: "160",
+        rating: 5
     },
     {
-        subject:"general_studies",
-        thumbnail:"course_2.png",
-        number_of_lessons:"120",
-        about_course:"Studying government, law, decision-making, equality, rights, fairness, global interactions, relationships, politics.",
-        tutor:"Salman Hyder",
-        prev_institude:"Drishti IAS",
-        student_enrolled:"160",
-        rating:5
+        subject: "General studies 1",
+        thumbnail: "course_2.png",
+        number_of_lessons: "120",
+        about_course: "Studying government, law, decision-making, equality, rights, fairness, global interactions, relationships, politics.",
+        tutor: "Salman Hyder",
+        prev_institude: "Drishti IAS",
+        student_enrolled: "160",
+        rating: 5
     },
     {
-        subject:"general_studies",
-        thumbnail:"course_3.png",
-        number_of_lessons:"120",
-        about_course:"Exploring economic growth, agriculture, science, environment, security, and disaster management to promote sustainable development and safety.",
-        tutor:"Ananta Shroff",
-        prev_institude:"Edukemy IAS",
-        student_enrolled:"200",
-        rating:5
+        subject: "General studies 1",
+        thumbnail: "course_3.png",
+        number_of_lessons: "120",
+        about_course: "Exploring economic growth, agriculture, science, environment, security, and disaster management to promote sustainable development and safety.",
+        tutor: "Ananta Shroff",
+        prev_institude: "Edukemy IAS",
+        student_enrolled: "200",
+        rating: 5
     },
     {
-        subject:"general_studies",
-        thumbnail:"course_1.png",
-        number_of_lessons:"120",
-        about_course:"Examining ethical principles, attitudes, and values, illustrated through case studies to promote probity in governance and decision-making.",
-        tutor:"Himanshu Kandpal",
-        prev_institude:"TestBook",
-        student_enrolled:"200",
-        rating:5
+        subject: "General studies 1",
+        thumbnail: "course_1.png",
+        number_of_lessons: "120",
+        about_course: "Examining ethical principles, attitudes, and values, illustrated through case studies to promote probity in governance and decision-making.",
+        tutor: "Himanshu Kandpal",
+        prev_institude: "TestBook",
+        student_enrolled: "200",
+        rating: 5
     },
     {
-        subject:"CSAT",
-        thumbnail:"course_2.png",
-        number_of_lessons:"120",
-        about_course:"Studying diverse art, modern &world history, post-Independence India, world & Indian geography, and Indian society's culture and structure.",
-        tutor:"Samriti talk",
-        prev_institude:"Drishti IAS",
-        student_enrolled:"160",
-        rating:5
+        subject: "CSAT",
+        thumbnail: "course_2.png",
+        number_of_lessons: "120",
+        about_course: "Studying diverse art, modern &world history, post-Independence India, world & Indian geography, and Indian society's culture and structure.",
+        tutor: "Samriti talk",
+        prev_institude: "Drishti IAS",
+        student_enrolled: "160",
+        rating: 5
     }
 ]
 
 testimonials = [
     {
-        name:"Adam Smith",
-        Designation:"Python Developer",
-        tesimonial:"Testimonial 1  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        rating:4,
+        name: "Adam Smith",
+        Designation: "Python Developer",
+        tesimonial: "Testimonial 1  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        rating: 4,
     },
     {
-        name:"Avadh Ojha",
-        Designation:"Modern World History Professor",
-        tesimonial:"Testimonial 2  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        rating:4,
+        name: "Avadh Ojha",
+        Designation: "Modern World History Professor",
+        tesimonial: "Testimonial 2  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        rating: 4,
     },
     {
-        name:" Dr. Vikas Divyakirti",
-        Designation:"CEO Drishti IAS",
-        tesimonial:"Testimonial 3  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        rating:4,
+        name: " Dr. Vikas Divyakirti",
+        Designation: "CEO Drishti IAS",
+        tesimonial: "Testimonial 3  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        rating: 4,
     },
     {
-        name:"B. Singh Sir",
-        Designation:"Ex. IES",
-        tesimonial:"Testimonial 4  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        rating:4,
+        name: "B. Singh Sir",
+        Designation: "Ex. IES",
+        tesimonial: "Testimonial 4  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        rating: 4,
     },
     {
-        name:"Kapuria Sir",
-        Designation:"Faculty for Indian Economy",
-        tesimonial:"Testimonial 5  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        rating:4,
+        name: "Kapuria Sir",
+        Designation: "Faculty for Indian Economy",
+        tesimonial: "Testimonial 5  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        rating: 4,
     },
     {
-        name:"Vibhas Jha Sir",
-        Designation:"Assistant Professor at Delhi School of Economics",
-        tesimonial:"Testimonial 6  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        rating:4,
+        name: "Vibhas Jha Sir",
+        Designation: "Assistant Professor at Delhi School of Economics",
+        tesimonial: "Testimonial 6  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        rating: 4,
     }
 ]
 
+let saveData = null
+let currentCourse = "General Studies";
 
-
-app.get('/', function(req, res){
-    res.render('index.ejs', {menu:menu, url:req.url});
+app.get('/', async function (req, res) {
+    let response;
+    if (saveData == null) {
+        response = await Client.get('course-categories')
+        saveData = response.data
+        // console.log("🚀 ~ file: index.js:161 ~ saveData:", saveData)
+    }
+    console.log("🚀 ~ file: index.js:162 ~ req.query:", req.query);
+    if (req.query.course && Object.keys(saveData).filter(i => i == req.query.course).length > 0) {
+        currentCourse = req.query.course;
+    }
+    req.session.data = saveData[currentCourse]
+    // Set the initial currentCourse value
+    res.render('index.ejs', { menu: menu, url: req.url, courses: saveData, currentCourse });
 });
 
-app.get('/course', function(req, res){
-    res.render('course.ejs', {menu:menu, url:req.url, courses:courses});
-});
-
-
-app.get('/course_detail', (req,res)=>{
-    res.render('course_detail.ejs', {menu:menu, url:req.url, syllabusData:syllabusData})
+app.get('/course_detail/:course/:id', async (req, res) => {
+    if (saveData == null) {
+        response = await Client.get('course-categories')
+        saveData = response.data
+        // console.log("🚀 ~ file: index.js:161 ~ saveData:", saveData)
+    }
+    const parser = new edjsParser();
+    console.log("🚀 ~ file: index.js:190 ~ app.get ~ req.params.course:", req.query)
+    // console.log("🚀 ~ file: index.js:191 ~ app.get ~ saveData:", saveData)
+    let data = saveData[req.params.course].filter(i => i.id == req.query.id)[0]
+    // console.log("🚀 ~ file: index.js:190 ~ app.get ~ data:", data)
+    let syllabusData = data.course_infos[0].Syllabus.weeks
+    let StudyMaterial = parser.parse(data.course_infos[0].StudyMaterial)
+    let CourseOutline = parser.parse(data.course_infos[0].CourseOutline)
+    res.render('course_detail.ejs', { menu: menu, url: req.url, syllabusData: syllabusData, CourseOutline, StudyMaterial, })
 })
+
+app.get('/course', function (req, res) {
+    res.render('course.ejs', { menu: menu, url: req.url, courses: courses });
+});
+
+
+
 
 
 // COURSE DATA LINKS
-app.get('/courses/:course_name', (req,res) => {
+app.get('/courses/:course_name', (req, res) => {
     const filteredArray = courses.filter(obj => obj.subject === req.params.course_name);
     res.send(
         {
-            course_name:req.params.course_name,
-            courses:filteredArray
+            course_name: req.params.course_name,
+            courses: filteredArray
         })
 });
 
 
-app.get('/blog', function(req, res){
-    res.render('blog.ejs', {menu:menu, url:req.url});
+app.get('/blog', async (req, res) => {
+    let pagenumber = 1
+    let limit = 10
+    const blogs = await Client(`blogs?page=${pagenumber}&limit=${limit}`)
+    console.log("🚀 ~ file: index.js:225 ~ app.get ~ blogs:", blogs.data.data)
+
+    res.render('blog.ejs', { menu: menu, url: req.url, blogs: blogs.data.data, getTimeAgo });
 });
 
-app.get('/blog_content', (req,res) => {
-    res.render('blog_content.ejs', {menu:menu, url:req.url});
+app.get('/blog/:slug', async (req, res) => {
+    const blogs = await Client(`blog/${req.params.slug}`)
+    console.log("🚀 ~ file: index.js:225 ~ app.get ~ blogs:", blogs.data.data)
+
+
+    res.render('blog_content.ejs', { menu: menu, url: req.url, blog: blogs.data, getTimeAgo, editorJsParse });
 })
-app.get('/valuepropostion', function(req, res){
-    res.render('valueProp.ejs', {menu:menu, url:req.url});
+app.get('/valuepropostion', function (req, res) {
+    res.render('valueProp.ejs', { menu: menu, url: req.url });
 });
 
-app.get('/team', function(req, res){
-    res.render('team.ejs', {menu:menu, url:req.url});
+app.get('/team', function (req, res) {
+    res.render('team.ejs', { menu: menu, url: req.url });
 });
 
-app.get('/about', function(req, res){
-    res.render('about.ejs', {menu:menu, url:req.url});
+app.get('/about', function (req, res) {
+    res.render('about.ejs', { menu: menu, url: req.url });
 });
 
-app.get('/contact', function(req, res){
-    res.render('contact.ejs', {menu:menu, url:req.url});
+app.get('/contact', function (req, res) {
+    res.render('contact.ejs', { menu: menu, url: req.url });
 });
 
-const syllabusData = [{"weekNumber":"1","readings":[{"title":"handouts 1","time":"120m"},{"title":"topic pdf ","time":"2h"}],"videos":[{"title":"topic lecture ","time":"2h"},{"title":"spefic topic lecture 2","time":"3h"}],"tests":[{"title":"MCQ test ","time":"20m"},{"title":"Subjective test ","time":"2h"}]},{"weekNumber":"2","readings":[{"title":"handouts 1","time":"120m"},{"title":"topic pdf ","time":"2h"}],"videos":[],"tests":[]}];
+const syllabusDatas = [{ "weekNumber": "1", "readings": [{ "title": "handouts 1", "time": "120m" }, { "title": "topic pdf ", "time": "2h" }], "videos": [{ "title": "topic lecture ", "time": "2h" }, { "title": "spefic topic lecture 2", "time": "3h" }], "tests": [{ "title": "MCQ test ", "time": "20m" }, { "title": "Subjective test ", "time": "2h" }] }, { "weekNumber": "2", "readings": [{ "title": "handouts 1", "time": "120m" }, { "title": "topic pdf ", "time": "2h" }], "videos": [], "tests": [] }];
 
 
 
 
-app.get('/testimonials', (req,res)=>{
-    res.send({testimonials})
+app.get('/testimonials', (req, res) => {
+    res.send({ testimonials })
 })
 
-app.get('/specific_course',(req,res) => {
-    res.send({message:'ok'})
+app.get('/specific_course', (req, res) => {
+    res.send({ message: 'ok' })
 })
 
 const port = process.env.PORT
-app.listen(port || 8000, () => {console.log(`server is running on the port ${port}`)})
+app.listen(port || 8000, () => { console.log(`server is running on the port ${port}`) })
