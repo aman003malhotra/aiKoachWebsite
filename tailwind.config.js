cdcfb8d947103ca055025f6639dfceda1de22616
manishbajpai055 <manishbajpai055@gmail.com>
2023-06-16 22:21:23 +0530

    blogs, details blog use slug, parsers , tailwind cofig with postcss removed cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: [
     "./public/**/*.{html,js,css}",
     "./src/**/*.{html,js,css}",
+    "./views/*.{html,js,ejs}",
+    "./views/**/*.{html,js,ejs}",
     "./node_modules/tw-elements/dist/js/**/*.js",
+    "./node_modules/editorjs-parser/build/Parser.node.js",
   ],
-  plugins: [require("tw-elements/dist/plugin.cjs")]
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  corePlugins: {
+    preflight: true,
+    tableLayout: true
   }
+}
