af257a309c7a9b267239dd4aebc27f2027d4ed09
manishbajpai055 <manishbajpai055@gmail.com>
2023-07-18 20:38:35 +0530

    fixes hero section

--- public/main.js
+++ public/main.js
@@ -16,18 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    new Splide("#course_splide", {
-      perPage: 1,
-      perMove: 1,
-      rewind: !0,
+  new Splide("#course_splide", {
+    perPage: 1,
+    perMove: 1,
+    rewind: !0,
     //   gap: 5,
-      autoplay: !0,
-      pauseOnHover: !0,
+    autoplay: !0,
+    pauseOnHover: !0,
     //   breakpoints: {
     //     640: { perPage: 1 },
     //     768: { perPage: 2 },
     //     1024: { perPage: 3 },
     //   },
-    }).mount();
-  });
-  
+  }).mount();
+});
